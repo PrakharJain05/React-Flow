1b8d041a4c5c3b8672d7b5f515e9dbcae686be3c
Prakhar Jain <prakharj05@gmail.com>
2021-06-07 17:02:21 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-// @flow
-import logo from "./logo.svg";
+//@flow
+import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 
 function App(): React$Element<"div"> {
+  const [image, setImage] = useState("");
+  function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  const getImage = async () => {
+    await axios(
+      `https://jsonplaceholder.typicode.com/photos/${randomNumber(1, 4998)}`
+    ).then((response) => setImage(response.data.thumbnailUrl));
+  };
+  useEffect(() => {
+    getImage();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={image} alt="logo" />
+        <button onClick={() => getImage()}>Get Image</button>
       </header>
     </div>
   );
